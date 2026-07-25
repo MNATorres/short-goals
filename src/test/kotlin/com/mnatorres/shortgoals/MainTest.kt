4ac2a497d02d72a951b61cbf76508ebadbc838d4
Matias Torres <mtri21019@gmail.com>
2026-07-25 15:39:00 -0300

    Add test setup with kotlin.test

---
+++ src/test/kotlin/com/mnatorres/shortgoals/MainTest.kt
@@ -0,0 +1,12 @@
+package com.mnatorres.shortgoals
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class MainTest {
+
+    @Test
+    fun `test setup runs`() {
+        assertTrue(true)
+    }
+}
